c6e71f7077d9caf471afb503856c4c2178aa1415
Edward Crowder <eddy.crowder@gmail.com>
2019-04-01 17:46:53 -0400

    updated add_ecp to support p2p addition

--- plot_EC.py
+++ plot_EC.py
@@ -8,9 +8,10 @@ A = 0;
 B = 0;
 P = 0;
 
-def addp2p(i,p,q):
-    p = vectors.Point(15,6,0)
-    print(checkPoint(p))
+def addp2p(i,p):
+    q = p
+    r = add_ecp(p,q)
+    print(r)
 
 def checkPoint(p):
     print(p)
@@ -25,13 +26,14 @@ def add_ecp(p,q):
     if (checkPoint(p) and checkPoint(q)):
         λ = get_delta(p,q)
         Xr = get_Xr(λ, p.x, q.x)
-        Yr = get_Yr(λ, p.x, Xr, q.y)
+        Yr = get_Yr(λ, p.x, Xr, p.y)
         r = vectors.Point(Xr,Yr,0)
         if(checkPoint(r)):
             #annotation = "({:d},{:d}) + ({:d},{:d}) = ({:d},{:d})".format(xp, yp, xq,yq,Xr,Yr)
             print(r.x)
             print(r.y)
             plt.plot(r.x, r.y, "or")
+            return r
         else:
             print("Something went wrong....")
     else:
@@ -41,7 +43,12 @@ def add_ecp(p,q):
 def get_delta(p,q):
     if (p.x == q.x and p.y == q.y):
         #calculate same point delta using differiential calculus
-        λ = ( (3* pow(p.x,2) + A) / (2*p.y))
+        num = (3 * pow(p.x, 2) + A) % P
+        den = (2*p.y)
+        r1 = den / num
+        r2 = get_Inverse(r1)
+        λ = 1*(r2) % P
+        #λ = ( (3* pow(p.x,2) + A) / (2*p.y))
     else:
         #calculate delta with rise over run
         num = (q.y - p.y)
@@ -54,9 +61,9 @@ def get_Xr(λ, xp, xq):
     return ((pow(λ, 2) - xp -xq) % P)
 
 def get_Yr(λ,xp,xr,yp):
-    step1 = (xp - xr)
+    step1 = (int(round(xp) - int(round(xr))))
     step2 = (λ * step1)
-    step3 = (step2 - yp)
+    step3 = (step2 - int(round(yp)))
     step4 = step3 % P
     return step4
     #return ((λ (xp - xr) -yp) % P)
@@ -90,12 +97,15 @@ def plot_ec(p,a,b ):
     return plt
 
 # plot ec: Ep(A,B) format and set global A and B for other functions
-plot_ec(23,1,4)
+plot_ec(11,1,1)
 #Print Globals
 #print(P)
 #print(A)
 #print(B)
-#add_ecp(8,8,13,11)   # Return same
-addp2p(2,8,8)
+p = vectors.Point(3,8,0)
+q = vectors.Point(3,8,0)
+add_ecp(p,q)   # Return same
+#p = vectors.Point(3,10,0)
+#addp2p(2,p)
 #display the graphs
 plt.show()
